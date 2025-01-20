c21f9f7dbd899d69c0062d07fe17ad31f623103b
tarasik21 <kyrisha2003@ukr.net>
2025-01-20 13:08:03 +0100

    Corr

--- l_13/task_2/calculator.js
+++ l_13/task_2/calculator.js
@@ -1,6 +1,9 @@
-export const getSquaredNumbers = arr =>
-    arr.map(num => num * num);
-export const getOddNumbers = arr =>
-    arr.filter(num =>(num % 2 === 1));
-export default sum = (a , b) => a + b;
 
+export const getSquaredNumbers = arr => arr.map(num => num * num);
+
+
+export const getOddNumbers = arr => arr.filter(num => num % 2 === 1);
+
+
+const sum = (a, b) => a + b;
+export default sum;
