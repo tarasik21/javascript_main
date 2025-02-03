e3e7ada03ef50d7becee8da9db487f38aeef0aee
tarasik21 <kyrisha2003@ukr.net>
2025-02-03 21:15:07 +0100

    corr

--- l_21/task_4/index.js
+++ l_21/task_4/index.js
@@ -2,3 +2,17 @@ function getTitle() {
     const titleElem = document.querySelector('.title');
      return titleElem.textContent;
   }
+  function getDescription() {
+    const descriptionElem = document.querySelector('.about');
+     return descriptionElem.innerText;
+  }
+  function getPlans() {
+    const plansElem = document.querySelector('.plans');
+     return plansElem.innerHTML;
+  }
+  function getGoal() {
+    const goalElem = document.querySelector('.goal');
+     return goalElem.outerHTML;
+  }
+
+
