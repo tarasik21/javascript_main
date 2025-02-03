9c00edbf74872f2f47ca30b2ce76bbdf3a99c11d
tarasik21 <kyrisha2003@ukr.net>
2025-02-03 21:26:27 +0100

    corr

--- l_21/task_4/index.js
+++ l_21/task_4/index.js
@@ -1,18 +1,20 @@
 function getTitle() {
     const titleElem = document.querySelector('.title');
-     return titleElem.textContent;
-  }
-  function getDescription() {
-    const descriptionElem = document.querySelector('.about');
-     return descriptionElem.innerText;
-  }
-  function getPlans() {
-    const plansElem = document.querySelector('.plans');
-     return plansElem.innerHTML;
-  }
-  function getGoal() {
-    const goalElem = document.querySelector('.goal');
-     return goalElem.outerHTML;
-  }
+    return titleElem ? titleElem.textContent : ''; // Если элемент найден, вернуть текст, иначе пустую строку
+}
 
+function getDescription() {
+    const descriptionElem = document.querySelector('.about');
+    return descriptionElem ? descriptionElem.innerText : '';
+}
+
+function getPlans() {
+    const plansElem = document.querySelector('.plans');
+    return plansElem ? plansElem.innerHTML : '';
+}
+
+function getGoal() {
+    const goalElem = document.querySelector('.goal');
+    return goalElem ? goalElem.outerHTML : '';
+}
 
