b2cec93bd3f97a60a3c131125f9b4cd572b776c2
tarasik21 <kyrisha2003@ukr.net>
2025-02-03 17:43:52 +0100

    corr

---
+++ l_21/task_2/index.js
@@ -0,0 +1,10 @@
+export function getTitleElement() {
+    const titleElem = document.querySelector('.title'); 
+    console.dir(titleElem); 
+    return titleElem;
+  }
+  export function getInputElement() {
+    const inputElem = document.querySelector('.inputElem'); 
+    console.dir(inputElem); 
+    return inputElem;
+  }
