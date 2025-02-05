3c1de4e5ee09e1200315223612947911e5906568
tarasik21 <kyrisha2003@ukr.net>
2025-02-05 19:18:57 +0100

    corr

--- l_22/task_1/index.js
+++ l_22/task_1/index.js
@@ -1,3 +1,12 @@
+const logTarget = (text, color) => {
+    const eventsList = document.querySelector('.events-list');
+    const span = document.createElement('span');
+    span.style.color = color;
+    span.style.marginLeft = '8px';
+    span.textContent = text;
+    eventsList.appendChild(span);
+};
+
 const attachHandlers = () => {
     document.querySelector('.rect_div')
         .addEventListener('click', () => logTarget('div', 'green'), false);
@@ -13,3 +22,33 @@ const attachHandlers = () => {
     document.querySelector('.rect_span')
         .addEventListener('click', () => logTarget('span', 'grey'), true);
 };
+
+const removeHandlers = () => {
+    document.querySelector('.rect_div')
+        .removeEventListener('click', () => logTarget('div', 'green'), false);
+    document.querySelector('.rect_p')
+        .removeEventListener('click', () => logTarget('p', 'green'), false);
+    document.querySelector('.rect_span')
+        .removeEventListener('click', () => logTarget('span', 'green'), false);
+
+    document.querySelector('.rect_div')
+        .removeEventListener('click', () => logTarget('div', 'grey'), true);
+    document.querySelector('.rect_p')
+        .removeEventListener('click', () => logTarget('p', 'grey'), true);
+    document.querySelector('.rect_span')
+        .removeEventListener('click', () => logTarget('span', 'grey'), true);
+};
+
+const clearEvents = () => {
+    document.querySelector('.events-list').innerHTML = '';
+};
+
+document.querySelector('.attach-handlers-btn')
+    .addEventListener('click', attachHandlers);
+
+document.querySelector('.remove-handlers-btn')
+    .addEventListener('click', removeHandlers);
+
+document.querySelector('.clear-btn')
+    .addEventListener('click', clearEvents);
+
