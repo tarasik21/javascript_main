eed56235d99b001dba39d2f217a5894587a9978e
tarasik21 <Kyrisha2003@ukr.net>
2025-02-08 15:41:37 +0100

    corr

--- l_22/task_7/index.js
+++ l_22/task_7/index.js
@@ -1,7 +1,11 @@
-const buttons = document.querySelectorAll('.btn'); 
-
-buttons.forEach(function(button) {
+function addClickHandler(button) {
     button.addEventListener('click', function handleClick(event) {
         console.log(event.target.textContent); 
     });
-});
+}
+
+
+const buttons = document.querySelectorAll('.btn');
+
+
+buttons.forEach(addClickHandler);
