cf778b9e8769d4f8e28bbdb650462c7a5a2f889e
tarasik21 <Kyrisha2003@ukr.net>
2025-02-08 14:18:44 +0100

    corr

--- l_22/task_4/index.js
+++ l_22/task_4/index.js
@@ -1,9 +1,13 @@
 
-const inputElem = document.querySelector('.text-input'); 
-const statusElem = document.querySelector('.task-status'); 
+document.addEventListener("DOMContentLoaded", function () {
 
+    const inputElem = document.querySelector('.text-input');
+    const statusElem = document.querySelector('.task-status');
+    
 
-inputElem.addEventListener("input", function() {
+    inputElem.addEventListener("input", function() {
 
-  console.log("Checkbox checked:", statusElem.checked);
-});
+      console.log("Checkbox checked:", statusElem.checked);
+    });
+  });
+  
