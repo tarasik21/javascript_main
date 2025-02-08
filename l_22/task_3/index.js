fd44d644fa095a7cad582f4f894be920d4e2a12b
tarasik21 <Kyrisha2003@ukr.net>
2025-02-08 13:57:36 +0100

    corr

--- l_22/task_3/index.js
+++ l_22/task_3/index.js
@@ -3,3 +3,6 @@ const inputElem = document.querySelector('.text-input');
 inputElem.addEventListener("input", function() {
     console.log("Current input: " + inputElem.value);
   });
+  inputElem.addEventListener("change", function() {
+    alert("Значение изменилось: " + inputElem.value);
+  });
