71e3706f176f5693ab744d8781f2e9766134598c
tarasik21 <Kyrisha2003@ukr.net>
2025-02-08 14:14:37 +0100

    corr

--- l_22/task_4/index.js
+++ l_22/task_4/index.js
@@ -1,4 +1,5 @@
 const statusElem = document.querySelector('.task_status'); 
+const inputElem = document.querySelector('.text-input')
 
 statusElem.addEventListener("change", function() {
   console.log(statusElem.checked); 
