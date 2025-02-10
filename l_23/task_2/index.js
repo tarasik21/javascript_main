1840049f56b784451187df164ac738c6d89280c1
tarasik21 <kyrisha2003@ukr.net>
2025-02-10 18:51:08 +0100

    corr

--- l_23/task_2/index.js
+++ l_23/task_2/index.js
@@ -25,8 +25,11 @@ const getSectorLines = () =>{
         >${seatsString}</div>
         ').join('');
 }
+
+
+const arenaElem = document.querySelector('.arena');
+
 const renderArena = () => {
-    const arenaElem = document.querySelector('.arena');
     const linesString = getSectorLines();
 
     const sectorsString = generateNumbersRange(1, 3)
@@ -39,5 +42,23 @@ const renderArena = () => {
 
                 arenaElem.innerHTML = sectorsString;
 
-}
+};
+
+const onSeatSelect = event =>{
+const isSeat = event.target.classList.contains('sector__seat');
+
+    if(!isSeat){
+        return;
+    }
+
+    const seatNumber = event.target.dataset.seatNumber;
+    const lineNumber = enent.target.closest('.sector__line').dataset.lineNumber;
+    const sectorNumber = enent.target.closest('.sector').dataset.sectorNumber;
+
+    const selectedSeatElem = document.querySelector('.board__selected-seat');
+    selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
+};
+
+arenaElem.addEventListener('click', onSeatSelect);
+
 renderArena();
