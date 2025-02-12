dc8e22ffc6b4fc7a833881f81f4a6e270a13ffc3
tarasik21 <Kyrisha2003@ukr.net>
2025-02-12 17:17:05 +0100

    corr

--- l_24/task_4/index.js
+++ l_24/task_4/index.js
@@ -1,9 +1,18 @@
-export function studentsBirthdays(students) {
+export function studentsBirthDays(students) {
 
     students.sort((a, b) => new Date(a.birthDate) - new Date(b.birthDate));
 
 
-    return students.map(({ name, birthDate }) => 
-        `name: ${name}, birthDate: ${birthDate}`
-    );
+    const groupedByMonth = students.reduce((acc, { name, birthDate }) => {
+        const month = new Date(birthDate).toLocaleString('en', { month: 'long' });
+
+        if (!acc[month]) {
+            acc[month] = [];
+        }
+
+        acc[month].push(name);
+        return acc;
+    }, {});
+
+    return groupedByMonth;
 }
