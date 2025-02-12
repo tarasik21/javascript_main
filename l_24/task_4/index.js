53fa817452ec61c081a7147edebe2ec4a36b79e2
tarasik21 <Kyrisha2003@ukr.net>
2025-02-12 17:21:42 +0100

    corr

--- l_24/task_4/index.js
+++ l_24/task_4/index.js
@@ -1,10 +1,24 @@
 export function studentsBirthDays(students) {
 
-    students.sort((a, b) => new Date(b.birthDate) - new Date(a.birthDate));
+    const sortedStudents = [...students];
 
 
-    const groupedByMonth = students.reduce((acc, { name, birthDate }) => {
-        const month = new Date(birthDate).toLocaleString('en', { month: 'long' });
+    sortedStudents.sort((a, b) => {
+        const dayA = Number(a.birthDate.split('/')[1]);
+        const dayB = Number(b.birthDate.split('/')[1]);
+        return dayA - dayB;
+    });
+
+    const groupedByMonth = sortedStudents.reduce((acc, { name, birthDate }) => {
+
+        const monthNum = birthDate.split('/')[0];
+        const monthNames = {
+            '01': 'Jan', '02': 'Feb', '03': 'Mar',
+            '04': 'Apr', '05': 'May', '06': 'Jun',
+            '07': 'Jul', '08': 'Aug', '09': 'Sep',
+            '10': 'Oct', '11': 'Nov', '12': 'Dec'
+        };
+        const month = monthNames[monthNum];
 
         if (!acc[month]) {
             acc[month] = [];
